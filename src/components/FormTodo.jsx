03d14493a77542c57965014496ab7f0b52ecaf36
Rick Hdz <rickhdz@Ricks-MacBook-Pro.local>
2022-10-06 19:38:51 -0500

    Form ready

--- src/components/FormTodo.jsx
+++ src/components/FormTodo.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React , { useState } from "react";
 
-const FormTodo = () => {
-	return <div>FormTodo!</div>;
-};
+const FormTodo = props => {
+	const [description, setDescription] = useState("");
+	const { handleAddItem } = props;
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		console.log(description);
+		handleAddItem({
+			done: false,
+			id: (+new Date()).toString(),
+			description
+		  });		
+		setDescription("");
+	  };	
+	return (
+	  <form onSubmit={handleSubmit}>
+
+		<div className="todo-list">
+		  <div className="file-input">
+			<input
+			  type="text"
+			  className="text"
+			  value={description}
+			  onChange={e => setDescription(e.target.value)}
+			/>
+			
+			<button
+			  className="button pink"
+			  disabled={description ? "" : "disabled"}
+			>
+			  Add
+			</button>
+		  </div>
+		</div>
+	  </form>
+	);
+  };
 
 export default FormTodo;
